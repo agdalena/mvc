1394167be55466c4c9f32dda337cfecac14be8b7
Rawenvoys <lukwis95@gmail.com>
2016-11-02 23:13:47 +0100

    Init

---
+++ Biblioteka/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Biblioteka.Models;
+using Biblioteka.Helper;
+
+namespace Biblioteka.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Account account)
+        {
+            using (DatabaseEntities dE = new DatabaseEntities())
+            {
+                var user = dE.AccountSet.Single(u => u.Login == account.Login && u.Password == account.Password);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", Consts.LOGIN_ERROR);
+                    return View();
+                }
+                Session[Consts.SESSION_ID] = user.IdAcount.ToString();
+                Session[Consts.SESSION_LOGIN] = user.Login.ToString();
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Account acc)
+        {
+            if (!ModelState.IsValid)
+                ModelState.Clear();
+            acc.Permission = "User";
+            using (DatabaseEntities dE = new DatabaseEntities())
+            {
+               
+                dE.AccountSet.Add(acc);
+                dE.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ Biblioteka/Helper/Methods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Biblioteka.Helper
+{
+    public class Methods
+    {
+        public static bool CheckIfUserIsAdmin()
+        {
+            if (HttpContext.Current.Session[Consts.SESSION_LOGIN] == null)
+                return false;
+            return true;
+        }
+    }
+}
